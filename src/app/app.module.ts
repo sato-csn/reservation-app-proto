e6136872932d679e3fd21bbc73eea77752955352
Sato Takashi <sato.csn@outlook.jp>
2023-07-05 16:57:38 +0900

    Angularフロントエンド側作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
+
+
+
 
 @NgModule({
   declarations: [
---
+++ src/app/product/products.ts
@@ -0,0 +1,52 @@
+export const products = [
+  { coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    heding1:'サンプルテキスト1',
+    heding2:'サンプルテキスト2',
+    heding3:'サンプルテキスト3',
+    headingtext1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+    headingtext2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+    headingtext3: 'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+    heding1:'サンプルテキスト1',
+    heding2:'サンプルテキスト2',
+    heding3:'サンプルテキスト3',
+    headingtext1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+    headingtext2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+    headingtext3: 'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+    heding1:'サンプルテキスト1',
+    heding2:'サンプルテキスト2',
+    heding3:'サンプルテキスト3',
+    headingtext1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+    headingtext2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+    headingtext3: 'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+  },
+  {
+    coverImage: './assets/img/phone-cover.jpg',
+    name: 'Phone Special',
+    price: 999,
+    description: '',
+    heding1:'サンプルテキスト1',
+    heding2:'サンプルテキスト2',
+    heding3:'サンプルテキスト3',
+    headingtext1: 'サンプルテキスト サンプルテキスト サンプルテキスト サンプルテキスト',
+    headingtext2: 'テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章 テキスト文章',
+    headingtext3: 'Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+
+  }
+];
