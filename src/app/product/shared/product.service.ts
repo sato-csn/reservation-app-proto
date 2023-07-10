9c0b12129b46c85c65cb5df9fb5ffba521455b95
Sato Takashi <sato.csn@outlook.jp>
2023-07-10 11:15:35 +0900

    結合追加分

---
+++ src/app/product/shared/product.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { products } from 'src/app/products';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any>{
+    //return products
+    return this.http.get('/api/v1/products')
+    //return this.http.get('http://localhost:3001/api/v1/products')
+  }
+
+  getProductById(productId: string): Observable<any>{
+    //return products[productId]
+    return this.http.get('/api/v1/products' + productId)
+  }
+
+}
